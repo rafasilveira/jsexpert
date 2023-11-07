cb20f8f0a959079d6d606d6c78fa0d45979a9525
Rafael Silveira <hashi@tiamat>
2023-11-07 09:53:19 -0300

    01-03: implement fibonacci sequence with recursion and generators

--- module01/03-spies/src/fibonacci.js
+++ module01/03-spies/src/fibonacci.js
@@ -1,14 +1,16 @@
 class Fibonacci {
   // Execute is a generator method (declared with the *), which means
   // it can "return" (yield) multiple values, without depending on
-  // workarounds such as returning an array. the method will yield 
+  // workarounds such as returning an array. the method will yield
   // its values on demand, when they become available.
   *execute(input, current = 0, next = 1) {
-    yield 0;
-    yield 1;
-    yield 1;
-    yield 2;
-    yield 3;
+    // all sequences processed, time to stop
+    if (input === 0) return;
+
+    yield current;
+
+    // delegates function execution but will not return value
+    yield* this.execute(input - 1, next, current + next);
   }
 }
 
--- module01/03-spies/src/fibonacci.test.js
+++ module01/03-spies/src/fibonacci.test.js
@@ -1,11 +1,13 @@
 // Fibonacci sequence: next number is the sum of its previous
-const { createSandbox } = require("sinon");
 
+const { createSandbox } = require("sinon");
 const Fibonacci = require("./fibonacci");
 
+const sinon = createSandbox();
 const fibonacci = new Fibonacci();
+
 (async () => {
-  for (const sequence of fibonacci.execute(5)) {
+  for (const sequence of fibonacci.execute(10)) {
     console.log({ sequence });
   }
 })();
